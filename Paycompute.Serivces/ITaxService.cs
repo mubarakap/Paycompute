be3e40482f788c09d0e3ee47e06a026375469d23
mubarakap <mubarakap@indellient.com>
2021-01-07 17:25:24 -0500

    TaxService - Interface and Implementation

---
+++ Paycompute.Serivces/ITaxService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paycompute.Serivces
+{
+    public interface ITaxService
+    {
+        decimal TaxAmount(decimal totalAmount);
+    }
+}
