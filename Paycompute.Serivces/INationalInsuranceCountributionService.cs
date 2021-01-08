aaab968b563a5f0035b56e92514d9a9e099b92a9
mubarakap <mubarakap@indellient.com>
2021-01-08 10:35:22 -0500

    NationalInsuranceContributionService - Interface and Implementation

---
+++ Paycompute.Serivces/INationalInsuranceCountributionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paycompute.Serivces
+{
+    public interface INationalInsuranceCountributionService
+    {
+        decimal NIContribution(decimal totalAmount);
+    }
+}
